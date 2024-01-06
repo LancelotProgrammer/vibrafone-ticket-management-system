a44817bb6c63dc2c9f79843374c0c6800daff52e
LancelotProgrammer <suhail.01248@gmail.com>
2024-01-06 17:51:35 +0300

    update the system

--- app/Filament/Resources/DepartmentResource/Pages/EditDepartment.php
+++ app/Filament/Resources/DepartmentResource/Pages/EditDepartment.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\DepartmentResource\Pages;
 
 use App\Filament\Resources\DepartmentResource;
+use App\Models\Ticket;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditDepartment extends EditRecord
 {
@@ -15,5 +17,32 @@ class EditDepartment extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        if ($record->title === $data['title'] || $record->code === $data['code']) {
+            $record->update($data);
+            return $record;
+        }
+        Ticket::where('department_id', $record->id)->chunk(10, function ($tickets) use ($record, $data) {
+            foreach ($tickets as $ticket) {
+                $newTicketIdentifier = preg_replace("/" . preg_quote($record->code, '/') . "/", $data['code'], $ticket->ticket_identifier, 1);
+                $ticket->update([
+                    'ticket_identifier' => $newTicketIdentifier,
+                ]);
+                $ticket->save();
+                $ticket->ticketHistory()->whereNotNull('work_order')->chunk(20, function ($ticketHistories) use ($record, $data) {
+                    foreach ($ticketHistories as $ticketHistory) {
+                        $newTiltle = preg_replace("/" . preg_quote($record->code, '/') . "/", $data['title'], $ticketHistory->title, 1);
+                        $ticketHistory->update([
+                            'title' => $newTiltle,
+                        ]);
+                    }
+                });
+            }
+        });
+        $record->update($data);
+        return $record;
     }
 }
